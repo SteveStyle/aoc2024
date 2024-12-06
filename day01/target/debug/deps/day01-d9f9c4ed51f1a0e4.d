494a6af270c1002644cf24fafe0673727b5c7a34
SteveStyle <stephenmor@gmail.com>
2024-12-06 18:36:27 +0000

    starting out

--- day01/target/debug/deps/day01-d9f9c4ed51f1a0e4.d
+++
@@ -1,9 +0,0 @@
-/home/steve/aoc2024/day01/target/debug/deps/libday01-d9f9c4ed51f1a0e4.rmeta: src/main.rs Cargo.toml
-
-/home/steve/aoc2024/day01/target/debug/deps/day01-d9f9c4ed51f1a0e4.d: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
