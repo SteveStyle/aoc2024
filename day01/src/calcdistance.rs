05cf7f000fed46beeb773439810892f84059c5d0
SteveStyle <stephenmor@gmail.com>
2024-12-09 16:18:13 +0000

    passing back duration

---
+++ day01/src/calcdistance.rs
@@ -0,0 +1,140 @@
+#![allow(dead_code)]
+use std::collections::HashMap;
+use std::time::{Duration, Instant};
+
+#[allow(dead_code)]
+const EXAMPLE: &str = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
+
+pub fn parse_input(input: &str) -> ((Vec<i32>, Vec<i32>), Duration) {
+    let now = Instant::now();
+    let mut x = Vec::new();
+    let mut y = Vec::new();
+    for line in input.lines() {
+        let mut it = line.split_whitespace().map(|n| n.parse());
+        if let (Some(Ok(xx)), Some(Ok(yy))) = (it.next(), it.next()) {
+            x.push(xx);
+            y.push(yy);
+        }
+    }
+    ((x, y), now.elapsed())
+}
+
+pub fn calc_distance(mut a: Vec<i32>, mut b: Vec<i32>) -> (i32, Duration) {
+    let now = Instant::now();
+    a.sort();
+    b.sort();
+    //println!("calc_distance: sort duration: {:?}", now.elapsed());
+    let ret = a.iter().zip(b).map(|(a, b)| (a - b).abs()).sum();
+    (ret, now.elapsed())
+}
+
+fn vec_to_count_map(v: Vec<i32>) -> HashMap<i32, i32> {
+    let mut map = HashMap::new();
+
+    for i in v {
+        let count = map.entry(i).or_insert(0);
+        *count += 1;
+    }
+
+    map
+}
+
+pub fn count_repeats1(a: Vec<i32>, b: Vec<i32>) -> (i32, Duration) {
+    let now = Instant::now();
+
+    let b_counts = vec_to_count_map(b);
+    let ret = a.iter().map(|i| i * b_counts.get(i).unwrap_or(&0)).sum();
+
+    (ret, now.elapsed())
+}
+
+pub fn count_repeats2(a: Vec<i32>, b: Vec<i32>) -> (i32, Duration) {
+    let now = Instant::now();
+
+    let a_counts = vec_to_count_map(a);
+    let b_counts = vec_to_count_map(b);
+    let ret = a_counts
+        .iter()
+        .map(|(i, count)| i * b_counts.get(i).unwrap_or(&0) * count)
+        .sum();
+
+    (ret, now.elapsed())
+}
+
+pub fn count_repeats3(mut a: Vec<i32>, mut b: Vec<i32>) -> (i32, Duration) {
+    let now = Instant::now();
+
+    a.sort();
+    b.sort();
+
+    let mut a_iter = a.into_iter();
+    let mut b_iter = b.into_iter();
+
+    let mut ret = 0;
+
+    let mut a_next = a_iter.next();
+    let mut b_next = b_iter.next();
+
+    while let (Some(a_val), Some(b_val)) = (a_next, b_next) {
+        match a_val.cmp(&b_val) {
+            std::cmp::Ordering::Equal => {
+                let a_count = {
+                    let mut count = 1;
+                    loop {
+                        a_next = a_iter.next();
+                        if a_next == Some(a_val) {
+                            count += 1;
+                        } else {
+                            break;
+                        }
+                    }
+                    count
+                };
+                let b_count = {
+                    let mut count = 1;
+                    loop {
+                        b_next = b_iter.next();
+                        if b_next == Some(b_val) {
+                            count += 1;
+                        } else {
+                            break;
+                        }
+                    }
+                    count
+                };
+                ret += a_val * a_count * b_count;
+            }
+            std::cmp::Ordering::Less => {
+                a_next = a_iter.next();
+            }
+            std::cmp::Ordering::Greater => {
+                b_next = b_iter.next();
+            }
+        }
+    }
+
+    (ret, now.elapsed())
+}
+
+#[allow(unused_imports)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_input() {
+        let ((a, b), _) = parse_input(EXAMPLE);
+        let (distance, _) = calc_distance(a, b);
+        assert_eq!(distance, 11);
+    }
+    #[test]
+    fn test_count_repeats() {
+        let ((a, b), _) = parse_input(EXAMPLE);
+        let (repeats, _) = count_repeats1(a, b);
+        assert_eq!(repeats, 31);
+    }
+}
--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,34 @@
+use std::time::Instant;
+
+mod calcdistance;
+
+mod timer;
+
+// import input.txt as a static string
+const INPUT: &str = include_str!("input.txt");
+
 fn main() {
-    println!("Hello, world!");
+    let now = Instant::now();
+
+    let ((a, b), parse_input_duration) = calcdistance::parse_input(INPUT);
+    let (distance, calc_distance_duration) = calcdistance::calc_distance(a.clone(), b.clone());
+
+    let (repeats1, count_repeats1_duration) = calcdistance::count_repeats1(a.clone(), b.clone());
+
+    let (repeats2, count_repeats2_duration) = calcdistance::count_repeats2(a.clone(), b.clone());
+
+    let (repeats3, count_repeats3_duration) = calcdistance::count_repeats3(a.clone(), b.clone());
+
+    let main_duration = now.elapsed();
+
+    println!("Distance: {}", distance);
+    println!("Repeats1: {}", repeats1);
+    println!("Repeats2: {}", repeats2);
+    println!("Repeats3: {}", repeats3);
+    println!("Parse input duration: {:?}", parse_input_duration);
+    println!("Calc distance duration: {:?}", calc_distance_duration);
+    println!("Count repeats1 duration: {:?}", count_repeats1_duration);
+    println!("Count repeats2 duration: {:?}", count_repeats2_duration);
+    println!("Count repeats3 duration: {:?}", count_repeats3_duration);
+    println!("Main duration: {:?}", main_duration);
 }
