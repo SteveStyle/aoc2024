d32d6859d7ee30d6f86f2faa02e3b2076373938c
SteveStyle <stephenmor@gmail.com>
2024-12-24 16:39:42 +0000

    day05 complete

---
+++ day05/src/main.rs
@@ -0,0 +1,46 @@
+use stephen_morris_utils::timer;
+
+const INPUT: &str = include_str!("input.txt");
+#[allow(dead_code)]
+const TESTINPUT: &str = "47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47";
+
+mod print_rules;
+
+fn main() {
+    let ru = timer::time(|| print_rules::parse_input(INPUT), "parse_input");
+
+    let sum = timer::time(|| print_rules::test_updates(&ru.0, &ru.1), "test_updates");
+
+    let reorder_sum = timer::time(|| print_rules::reorder(&ru.0, &ru.1), "reorder");
+
+    ru.print_duration();
+    sum.print_all();
+    reorder_sum.print_all();
+}
---
+++ day05/src/print_rules.rs
@@ -0,0 +1,105 @@
+//#![allow(dead_code, unused)]
+use std::{collections::HashMap, ops::Deref, ops::DerefMut};
+
+type Rules = Vec<(usize, usize)>;
+
+#[derive(Debug, Clone)]
+pub struct Update(Vec<usize>);
+
+impl Deref for Update {
+    type Target = Vec<usize>;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for Update {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+pub fn parse_input(input: &str) -> (Rules, Vec<Update>) {
+    let no_lines = input.lines().count();
+    let mut rules = Vec::with_capacity(no_lines >> 1);
+    let mut update_list = Vec::with_capacity(no_lines >> 1);
+    for line in input.lines() {
+        let line = line.trim();
+        if line.is_empty() {
+            continue;
+        };
+        if line.contains('|') {
+            let mut parts = line.split('|');
+            rules.push((
+                parts.next().unwrap().parse().unwrap(),
+                parts.next().unwrap().parse().unwrap(),
+            ));
+        } else {
+            update_list.push(Update(
+                line.split(',').map(|s| s.parse().unwrap()).collect(),
+            ))
+        }
+    }
+
+    (rules, update_list)
+}
+
+fn test_update<'a>(rules: &Rules, update: &'a Update) -> bool {
+    let mut index: [Option<usize>; 100] = [None; 100];
+    for (i, value) in update.0.iter().enumerate() {
+        index[*value] = Some(i);
+    }
+    for (a, b) in rules {
+        if let (Some(a_index), Some(b_index)) = (index[*a], index[*b]) {
+            if b_index < a_index {
+                return false;
+            };
+        }
+    }
+
+    true
+}
+
+pub fn test_updates(rules: &Rules, updates: &[Update]) -> usize {
+    updates
+        .iter()
+        .filter(|u| test_update(rules, u))
+        .map(|u| u[u.len() >> 1])
+        .sum()
+}
+
+pub fn reorder(rules: &Rules, updates: &[Update]) -> usize {
+    let mut hm_rules: HashMap<(usize, usize), std::cmp::Ordering> =
+        HashMap::with_capacity((rules.len() * (rules.len() - 1)) >> 1);
+    for (a, b) in rules {
+        hm_rules.insert((*a, *b), std::cmp::Ordering::Less);
+        hm_rules.insert((*b, *a), std::cmp::Ordering::Greater);
+    }
+    updates
+        .iter()
+        .filter(|u| !test_update(rules, u))
+        .map(|u| {
+            let mut u = u.clone();
+            u.sort_by(|a, b| *hm_rules.get(&(*a, *b)).unwrap());
+            u
+        })
+        .map(|u| u[u.len() >> 1])
+        .sum()
+}
+#[cfg(test)]
+mod tests {
+    use super::parse_input;
+    use super::test_updates;
+
+    #[test]
+    fn test_test_updates() {
+        let (rules, updates) = parse_input(crate::TESTINPUT);
+        assert_eq!(test_updates(&rules, &updates), 143);
+    }
+
+    #[test]
+    fn test_reorder() {
+        let (rules, updates) = parse_input(crate::TESTINPUT);
+        assert_eq!(super::reorder(&rules, &updates), 123);
+    }
+}
