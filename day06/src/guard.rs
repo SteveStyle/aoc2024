bbd735f0bbb00e0b56f5b28921c55e865d56cd89
¨Stephen <stephenmor@gmail.com>
2024-12-30 01:39:22 +0000

    day06 part 2 answered

--- day06/src/guard.rs
+++ day06/src/guard.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use stephen_morris_utils::pos::{Direction, };
 
 type Grid = crate::grid::Grid<u8>;
@@ -23,6 +25,7 @@ pub fn extract_guard(grid: &mut Grid) -> Option<Guard> {
     ret
 }
 
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 pub struct Guard {
     pos: Position,
     direction: Direction,
@@ -36,18 +39,81 @@ impl Guard {
         Self {pos: Position::new(col, row), direction}
     }
     pub fn move_once(&mut self, grid: &Grid) -> bool {
-        if grid.test_bounds( self.pos, self.direction) {
-         
-        let new_pos = self.pos + self.direction;
-        if *grid.get_pos(new_pos) == b'#' {
-            self.direction = self.direction.turn_right();
-        } else {
-            self.pos = new_pos;
-        }
-        true
+        if grid.test_bounds( self.pos, self.direction) {         
+            let new_pos = self.pos + self.direction;
+            if *grid.get_pos(new_pos) == b'#' {
+                self.direction = self.direction.turn_right();
+            } else {
+                self.pos = new_pos;
+            }
+            true
         } else {
             false
         }
     
+    }
+    pub fn count_guard_positions(&mut self, grid: &Grid) -> usize {
+        let mut guard_positions = Grid::new_default(grid.width, grid.height);
+        guard_positions.set_pos(self.pos, 1);
+        while self.move_once(grid) {
+            guard_positions.set_pos(self.pos, 1);
+        }
+        guard_positions.into_iter().map(|(_,_,v)| v as usize).sum()
+    }
+
+    pub fn count_blockers(&self, grid: &Grid) -> usize {
+        let mut guard_positions = Grid::new_default(grid.width, grid.height);
+
+        let mut guard_copy = *self;
+        let mut grid_copy = grid.clone();
+        
+        while guard_copy.move_once(grid) {
+            guard_positions.set_pos(guard_copy.pos, 1);
+        }
+
+        let position_count: usize = (&guard_positions).into_iter().map(|(_,_,v)| *v as usize).sum();
+        println!("{:?}", position_count);
+
+        let mut possible_blockers = Vec::new();
+
+        for (row, col, c) in &guard_positions {
+            if *c == 1 {
+                grid_copy.set(row, col, b'#');
+                guard_copy = *self;
+                let mut hs = HashSet::new();
+                while guard_copy.move_once(&grid_copy) {
+                    if !hs.insert(guard_copy.clone()) {
+                        possible_blockers.push((row, col));
+                        break;
+                    }
+                }
+                grid_copy.set(row, col, b'.');
+            }
+        }
+
+
+        possible_blockers.len()
+
+    }
+
+}
+
+    
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_all() {
+        let mut grid = parse_input(crate::TESTINPUT);
+        let mut guard = extract_guard(&mut grid).unwrap();
+        assert_eq!(guard.count_guard_positions(&grid), 41);
+    }
+
+    #[test]
+    fn test_blockers() {
+        let mut grid = parse_input(crate::TESTINPUT);
+        let mut guard = extract_guard(&mut grid).unwrap();
+        assert_eq!(guard.count_blockers(&grid), 6);
     }
 }
--- day06/src/main.rs
+++ day06/src/main.rs
@@ -20,5 +20,18 @@ mod guard;
 
 
 fn main() {
-    println!("Hello, world!");
+    let mut grid = timer::time(|| guard::parse_input(INPUT), "Parse input");
+    let mut guard = timer::time(|| guard::extract_guard(&mut grid), "Extract guard");
+    guard.print_all();
+    //grid.print_all();
+    let count = timer::time(|| guard.as_mut().unwrap().count_guard_positions(&grid), "Count guard positions");
+
+    grid.print_duration();
+    guard.print_all();
+    count.print_all();
+
+    let mut grid = timer::time(|| guard::parse_input(INPUT), "Parse input");
+    let mut guard = timer::time(|| guard::extract_guard(&mut grid), "Extract guard");
+    let count = timer::time(|| guard.as_mut().unwrap().count_blockers(&grid), "Count blockers");
+    count.print_all();
 }
